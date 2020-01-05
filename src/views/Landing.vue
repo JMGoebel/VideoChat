a0e88dde5646156dbdf3965089b00c26a8e8f30b
Jason Goebel <goebel.jason@gmail.com>
2020-01-05 12:26:46 -0600

    ADDS: Landing Page Layout

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="landing">
-    <h3>Landing Page</h3>
-  </div>
+  <v-layout>
+    <v-row no-gutter>
+      <v-col class="teal">
+        <v-layout column flex justify-space-between style="height: 100%;">
+          <div>City Chat</div>
+          <div>Image</div>
+        </v-layout>
+      </v-col>
+      <v-col class="blue-grey">
+        <v-layout column flex justify-space-between style="height: 100%;">
+          <div>Updates Component</div>
+          <div>Log-in component</div>
+          <div>User Quote Component</div>
+        </v-layout>
+      </v-col>
+    </v-row>
+  </v-layout>
 </template>
